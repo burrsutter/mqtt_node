ba5274d7a9201d8d76e60c7c489de16f5362d455
Burr Sutter <bsutter@redhat.com>
2015-04-04 15:29:13 -0400

    updated subscribe.js to match publish.js

--- subscribe.js
+++ subscribe.js
@@ -1,7 +1,7 @@
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://192.168.3.3');
 
-client.subscribe('mag_topic');
+client.subscribe('topic');
 
 client.on('message', function (topic, message) {
   // message is Buffer
